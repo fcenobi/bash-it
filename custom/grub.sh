cae3246ec3a79463a045b7542833bca12158e114
fcenobi <11414315+fcenobi@users.noreply.github.com>
2019-10-26 02:34:44 +0200

    Create grub.sh

---
+++ custom/grub.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#
+# This is an example file. Don't use this for your custom scripts. Instead, create another file within the
+# custom directory.
+alias updategrub='grub2-mkconfig -o /boot/grub2/grub.cfg'
